f99d31cca7251a36a18a00a31beb5adfd9c762e3
Acer <Acer@192.168.100.4>
2019-11-11 10:40:02 +0545

    file

--- FileHandling/src/file/FileOutputStreamExample.java
+++ FileHandling/src/file/FileOutputStreamExample.java
@@ -8,12 +8,12 @@ public class FileOutputStreamExample {
 	
 	public static void main(String[] args) {
 	
-	try {
+	try {     //Exception
 		FileOutputStream fout = new FileOutputStream("C:\\Users\\Acer\\Desktop\\stud.txt");
 		String str = "Java Developers in Nepal";
 		byte[] b = str.getBytes();
 		fout.write(b);
-		fout.close();
+		fout.close();    
 		System.out.println("Information is added in the file.");
 		
 	}catch(FileNotFoundException e) {
